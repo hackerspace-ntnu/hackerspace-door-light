2f0453f5f787c0bfd19663eb483c97195a6459e1
Geir Kulia <geir@kuila.no>
2014-08-29 15:28:01 +0200

    Initial implementation

---
+++ door_status_led.py
@@ -0,0 +1,54 @@
+# By Geir Kulia, 28. August 2014
+
+# Set up GPIO for LED
+import RPi.GPIO as GPIO
+import time, urllib2, json
+
+pin_number_LED = 7
+api_host = "http://hackerspace.idi.ntnu.no/api/door"
+
+def main():
+	GPIO.setup(pin_number_LED, GPIO.OUT)
+
+	while True:
+	# check if internet is connected
+	if internet_on():
+		# Write to GPIO
+		if checkDoor():
+			setLED(True)
+		else:
+			setLED(False)
+
+		time.sleep(10) # Delay 10 second
+	else:
+		# Blink if no connection
+		flashLED()
+
+def setLED(value):
+	GPIO.output(pin_number_LED, value)
+     
+def internet_on():
+	# check connection 
+    try:
+        response=urllib2.urlopen(api_host, timeout=1)
+        return True
+    except urllib2.URLError as err: pass
+    return False
+
+def checkDoor():
+	# Check if the door is open
+	response = urllib2.urlopen(api_host)
+	data = json.loads(response.read())
+
+	return data[0]['isOpen']
+
+def flashLED():
+	# Blink with the LED
+	setLED(True)
+	time.sleep(0.5)
+	setLED(False)
+	time.sleep(0.5)
+
+if __name__ == "__main__":
+	main()
+
