c8677082d56adfda2467a914692d81babf990342
Mathias Ose <mathias.ose@gmail.com>
2015-08-14 16:08:18 +0200

    Fix indent issue and refactor door_status_led.py

--- door_status_led.py
+++ door_status_led.py
@@ -1,47 +1,67 @@
 # By Geir Kulia, 28. August 2014
 
-# Set up GPIO for LED
-import RPi.GPIO as GPIO
-import time, urllib2, json
+import time
+import json
+import urllib2
 
-pin_number_LED = 7
-api_host = "http://hackerspace.idi.ntnu.no/api/door"
+import RPi.GPIO as GPIO
+
+
+SLEEP = 3
+
+LED_PIN = 7
+API_HOST = "http://hackerspace.idi.ntnu.no/api/door"
+
 
 def main():
-	GPIO.setup(pin_number_LED, GPIO.OUT)
+    GPIO.setup(LED_PIN, GPIO.OUT)
 
-	while True:
-	# check if internet is connected
-	if internet_on():
-		# Write to GPIO
-		if checkDoor():
-			setLED(True)
-		else:
-			setLED(False)
+    while True:
+        if is_internet_on():
+            if is_door_open():
+                led_on()
+            else:
+                led_off()
 
-		time.sleep(10) # Delay 10 second
-	else:
-		# Blink if no connection
-		setLED(False)
+            time.sleep(SLEEP)
+        else:
+            # Blink if no connection
+            for _ in range(max(1, SLEEP / 2)):
+                led_on()
+                time.sleep(1)
+                led_off()
+                time.sleep(1)
 
-def setLED(value):
-	GPIO.output(pin_number_LED, value)
-     
-def internet_on():
-	# check connection 
+
+def led_on():
+    GPIO.output(LED_PIN, True)
+
+
+def led_off():
+    GPIO.output(LED_PIN, False)
+
+
+def is_internet_on():
+    """Check connection to api_host"""
     try:
-        response=urllib2.urlopen(api_host, timeout=1)
+        urllib2.urlopen(API_HOST, timeout=1)
         return True
-    except urllib2.URLError as err: pass
+    except urllib2.URLError as err:
+        pass
     return False
 
-def checkDoor():
-	# Check if the door is open
-	response = urllib2.urlopen(api_host)
-	data = json.loads(response.read())
 
-	return data[0]['isOpen']
+def is_door_open():
+    """Check if the door is open"""
+    response = urllib2.urlopen(API_HOST)
+    data = json.loads(response.read())
+
+    if type(data) == list:
+        data = data[0]
+
+    return data['isOpen']['door']
+
 
 if __name__ == "__main__":
-	main()
+    main()
 
